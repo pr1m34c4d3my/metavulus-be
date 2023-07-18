539f793be86d76663a166c518763e15981d7229d
iqbalkicon <iqbalkicon@gmail.com>
2023-07-19 00:58:12 +0700

    Full API Account

--- controller/accountController.ts
+++ controller/accountController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import ACCOUNT from "../model/account";
+import { timeZone } from "../utils/timezone";
 
 const accountController = {
   createAccount: async (req: Request, res: Response) => {
@@ -33,6 +34,73 @@ const accountController = {
     try {
       const account = await ACCOUNT.find();
       res.status(200).json({ message: account });
+    } catch (error) {
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  },
+  getAccountByUsernameOrName: async (req: Request, res: Response) => {
+    const { username } = req.params;
+    try {
+      const account = await ACCOUNT.find({
+        $or: [
+          { username: { $regex: username, $options: "i" } },
+          { "accountDetail.fullName": { $regex: username, $options: "i" } },
+        ],
+      });
+      res.status(200).json({ message: account });
+    } catch (error) {
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  },
+  getAccountById: async (req: Request, res: Response) => {
+    const { Id } = req.params;
+    try {
+      const account = await ACCOUNT.find({
+        _id: Id,
+      });
+      res.status(200).json({ message: account });
+    } catch (error) {
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  },
+  updateAccount: async (req: Request, res: Response) => {
+    const { username, password, email, lastLogin, accountDetail, id } =
+      req.body;
+    const { fullName, mobileNumber, sex, birth, description, profilePicture } =
+      accountDetail;
+
+    try {
+      const updateAccount = {
+        username: username,
+        password: password,
+        email: email,
+        lastLogin: lastLogin,
+        accountDetail: {
+          fullName: fullName,
+          mobileNumber: mobileNumber,
+          sex: sex,
+          birth: birth,
+          description: description,
+          profilePicture: profilePicture,
+        },
+        updatedAt: timeZone(),
+      };
+      await ACCOUNT.findByIdAndUpdate(id, updateAccount);
+      res.status(200).json({
+        message: updateAccount,
+      });
+    } catch (error) {
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  },
+  accountDelete: async (req: Request, res: Response) => {
+    const { id } = req.body;
+
+    try {
+      await ACCOUNT.findByIdAndDelete(id);
+      res.status(200).json({
+        message: "Successfully Account Delete",
+      });
     } catch (error) {
       res.status(500).json({ message: "Internal Server Error" });
     }
--- router/handler/accountHandler.ts
+++ router/handler/accountHandler.ts
@@ -2,10 +2,17 @@ import express, { Router } from "express";
 const {
   createAccount,
   getAllAccount,
+  getAccountByUsernameOrName,
+  getAccountById,
+  updateAccount,
+  accountDelete,
 } = require("../../controller/accountController");
 
 const router: Router = express.Router();
 router.get("/", getAllAccount);
 router.post("/", createAccount);
-
+router.get("/getAccountByUsernameOrName/:username", getAccountByUsernameOrName);
+router.get("/getAccountById/:Id", getAccountById);
+router.patch("/", updateAccount);
+router.delete("/", accountDelete);
 module.exports = router;
